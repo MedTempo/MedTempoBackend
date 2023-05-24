4762de2c094009e650ac9f87d0bb5974b2095fc6
Mateus Felipe Vieira <habdig7@gmail.com>
2023-05-24 16:08:42 -0300

    Cassandra + Cors added

---
+++ config/db/Database.ts
@@ -0,0 +1,54 @@
+import cassandra from "npm:cassandra-driver"
+import credential from "./credentials.ts"
+
+
+try{
+    const client = new cassandra.Client({
+
+        contactPoints: ["172.18.0.3:9042",],
+        localDataCenter: "datacenter1",
+        keyspace: "medtempo",
+    
+        /*
+        credentials: {
+            username: "habdig7@gmail.com",
+            password: "SaNtInHo1!1",
+        },
+        */
+    });
+}
+
+catch(err){
+    
+}
+
+await client.connect()
+
+let iniciardb = await Deno.readTextFile(`./config/db/SetTables.cql`);
+
+let parsed = iniciardb.match(/[^\;]+\;?|\;/g);
+
+console.log(parsed)
+
+if(parsed != undefined){
+    for (let i = 0; i < parsed.length; i++) {
+        await client.execute(await parsed[i]).then(res => console.log(res.rows))
+    }
+    console.log(`\n\n\ntodas as querys finalizadas \n\n\n`)
+}
+
+
+export default client
+
+/*docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cassandra*/
+
+
+/*
+
+docker run --rm -d --name cassandra --hostname cassandra --network cassandra cassandra                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            docker run --rm --network cassandra -e CQLSH_HOST=cassandra -e CQLSH_PORT=9042 -e CQLVERSION=3.4.5 nuvo/docker-cqlsh
+
+docker run --rm --network cassandra -e CQLSH_HOST=cassandra -e CQLSH_PORT=9042 -e CQLVERSION=3.4.5 nuvo/docker-cqlsh
+
+docker run --rm --network cassandra -e CQLSH_HOST=cassandra -e CQLSH_PORT=9042 -e CQLVERSION=3.4.6 nuvo/docker-cqlsh
+
+*/
--- trigger.ts
+++ trigger.ts
@@ -4,13 +4,17 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 
 import Roteador from "./config/Roteador.ts";
+import Cors from "./config/CORS.ts";
 
 const app = new Application()
 
 const porta = 7777
 
+app.use(Cors)
+
 app.use(Roteador.routes())
 app.use(Roteador.allowedMethods())
+
 
 app.addEventListener("listen", function({port, secure, hostname}){
     console.log(`O backend do MedTempo Está no Ar em ${secure?"http":"https"}://${hostname??"localhost"}:${port}`)
