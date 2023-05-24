bc5e7ef4758898630628a1e177a4c6fc197eab94
Mateus Felipe Vieira <habdig7@gmail.com>
2023-05-24 08:55:13 -0300

    for deploy

--- config/Roteador.ts
+++ config/Roteador.ts
@@ -3,38 +3,47 @@ import { Router } from "https://deno.land/x/oak/mod.ts";
 
 const Roteador = new Router();
 
-let rotas: Array<any> = []
+import index from "../rotas/index.ts"
+
+
+
+let rotas: Array<any> = [
+    index
+]
 /*
 for await (const caminho of Deno.readDir("./rotas")) {
     rotas.push(await import(`../rotas/${caminho.name}`))
     //Roteador.get("/", );
 }*/
 
-
+/*
 for await (const caminho of Deno.readDir("./rotas")) {
     rotas.push(await import(`../rotas/${caminho.name}`))
     //Roteador.get(`/${i}`, rotas[i].default );
 }
+*/
+
+
 
 for (let i = 0; i < rotas.length; i++) {
 
-    for (let j = 0; j < rotas[i].default.length; j++) {
-        console.log(rotas[i].default[j]);
+    for (let j = 0; j < rotas[i].length; j++) {
+        console.log(rotas[i][j]);
 
-        if(rotas[i].default[j].metodo === `GET`){
-            Roteador.get(rotas[i].default[j].caminho, rotas[i].default[j].ctrl);
+        if(rotas[i][j].metodo === `GET`){
+            Roteador.get(rotas[i][j].caminho, rotas[i][j].ctrl);
         }
-        else if(rotas[i].default[j].metodo === `POST` ){
-            Roteador.post(rotas[i].default[j].caminho, rotas[i].default[j].ctrl);
+        else if(rotas[i][j].metodo === `POST` ){
+            Roteador.post(rotas[i][j].caminho, rotas[i][j].ctrl);
         }
 
     }
 
-    //console.log(rotas[i].default[0])    
+    //console.log(rotas[i].[0])    
 }
 
 
-//console.log(rotas[i].default);
+//console.log(rotas[i].);
 
 
 export default Roteador;
